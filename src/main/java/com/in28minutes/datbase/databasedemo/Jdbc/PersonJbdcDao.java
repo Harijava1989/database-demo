a81a501738f4c7cd7dbe3e98f419b4bda7c36df9
Basani Rakesh <t.harikrishna1989@gmail.com>
2022-02-05 12:48:40 +0530

    add

---
+++ src/main/java/com/in28minutes/datbase/databasedemo/Jdbc/PersonJbdcDao.java
@@ -0,0 +1,21 @@
+package com.in28minutes.datbase.databasedemo.Jdbc;
+
+import com.in28minutes.datbase.databasedemo.entity.Person;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.BeanPropertyRowMapper;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public class PersonJbdcDao {
+
+    @Autowired
+    JdbcTemplate jdbcTemplate;
+
+    public List<Person> findAll(){
+        return jdbcTemplate.query("select * from Person",
+                new BeanPropertyRowMapper<Person>(Person.class));
+    }
+}
